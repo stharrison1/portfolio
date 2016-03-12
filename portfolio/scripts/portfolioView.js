b08a1eaf0bed3a895411db32caaab689ba8d52ac
Susan Harrison <susan77harrison@gmail.com>
2016-03-11 20:24:32 -0800

    No changes trying to figure out if class-04 is merged with Master.

---
+++ portfolio/scripts/portfolioView.js
@@ -0,0 +1,79 @@
+var portfolioView = {};
+
+portfolioView.populateFilters = function() {
+  $('article').each(function() {
+    if (!$(this).hasClass('template')) {
+      var val = $(this).attr('data-category');
+      var optionTag = '<option value="' + val + '">' + val + '</option>';
+      if ($('#category-filter option[value="' + val + '"]').length === 0) {
+        $('#category-filter').append(optionTag);
+      }
+    }
+  });
+};
+
+portfolioView.handleCategoryFilter = function() {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="' + $(this).val() + '"]').fadeIn();
+    } else {
+      $('article').fadeIn(); //fadeOut ????
+      $('.template').hide();
+    }
+  });
+};
+
+portfolioView.handleMainNav = function () {
+  $('.main-nav').on('click', '.tab', function () {
+    $('.tab-content').hide();
+    $('#' + $(this).data('content')).fadeIn();
+  });
+  $('.main-nav .tab:first').click();
+};
+
+portfolioView.setTeasers = function() {
+  $('.project-body *:nth-of-type(n+2)').hide();
+
+  $('#projects a.read_on').on('click', function(event) {
+    event.preventDefault();
+    $(this).hide();
+    $(this).parent().find('*').show();
+  });
+};
+
+portfolioView.initNewArticlePage = function() {
+  $('.tab-content').show();
+  $('#export-field').hide();
+  $('article-json').on('focus', function() {
+    this.slect();
+  });
+  $('#new-form').on('change', 'input', 'textarea', portfolioView.create);
+};
+
+portfolioView.create = function() {
+  var article;
+  $('#articles').empty();
+  article = new Project({
+    title: $('#article-title').val(),
+    body: $('#article-body').val(),
+    // author: $('#article-author').val(),
+    repoUrl: $('#article-author-url').val(),
+    category: $('#article-category').val(),
+    publishedOn: $('#article-published:checked').length ? new Date() :null
+  });
+  $('#articles').append(article.toHtml());
+  $('#articles').each(function(i, block){
+    hljs.highlightBlock(block);
+  });
+  $('#export-field').show();
+  $('#article-json').val(JSON.stringify(article));
+};
+
+
+portfolioView.initIndexPage = function() {
+  portfolioView.populateFilters();
+  portfolioView.handleCategoryFilter();
+  portfolioView.handleMainNav();
+  portfolioView.setTeasers();
+};
