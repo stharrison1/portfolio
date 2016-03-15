467ff9c581c3c346d04cc49aa2d51c4ce358521f
Susan Harrison <susan77harrison@gmail.com>
2016-03-14 20:43:28 -0700

    Added AJAX, JSON, and eTag.

--- scripts/project.js
+++ scripts/project.js
@@ -1,27 +1,51 @@
-var projects = [];
-
-function Project(opts) {
+function Projects(opts) {
   for (key in opts) this[key] = opts[key];
 }
 
-Project.prototype.toHtml = function() {
-  var source = $('#projectData-template').html();
-  var template = Handlebars.compile(source);
+Projects.all = [];
+
+Projects.prototype.toHtml = function() {
+  // var source = $('#projectData-template').html();
+  var template = Handlebars.compile($('#projectData-template').text());
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  // this.body = marked(this.body); WHY DOESN'T THIS WORK?
   return template(this);
 };
 
-projectData.sort(function(a,b){
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
+Projects.loadAll = function(rawData) {
+  rawData.sort(function(a,b){
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  });
+  rawData.forEach(function(ele) {
+    Projects.all.push(new Projects(ele));
+  });
+};
+// projects.forEach(function(a){
+//   $('#portfolio').append(a.toHtml());
+// });
+//
+// $('.template').hide();
 
-projectData.forEach(function(ele) {
-  projects.push(new Project(ele));
-});
-
-projects.forEach(function(a){
-  $('#portfolio').append(a.toHtml());
-});
-
-$('.template').hide();
+Projects.fetchAll = function() {
+  var localEtag;
+  $.ajax({
+    type: 'HEAD',
+    url: 'scripts/projectData.json',
+    success: function(data, message, xhr) {
+      localEtag = xhr.getResponseHeader('eTag');
+      localStorage.setItem('eTag', localEtag);
+      if (localStorage.eTag === xhr.getResponseHeader('eTag') && localStorage.rawData) {
+        console.log(localStorage.eTag);
+        Projects.loadAll(JSON.parse(localStorage.rawData));
+        portfolioView.initIndexPage();
+      } else {
+        $.getJSON('scripts/projectData.json', function(rawData) {
+          Projects.loadAll(rawData);
+          localStorage.rawData = JSON.stringify(rawData);
+          portfolioView.initIndexPage();
+        });
+      }
+    }
+  });
+};
